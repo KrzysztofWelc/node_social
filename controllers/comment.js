b7caa81db84bce07e55fc5664d5d08a42627308f
Krzysztof <krzysiekwelc18@gmail.com>
2020-02-19 14:32:07 +0100

    adding comments

--- controllers/comment.js
+++ controllers/comment.js
@@ -3,8 +3,27 @@ const auth = require("../middleware/auth");
 const upload = require("../middleware/upload");
 const Comment = require("../db/models/Comment");
 
-router.post("/add", (req, res) => {
-  console.log("comment");
-});
+router.post(
+  "/add",
+  auth,
+  upload.single("image"),
+  async (req, res) => {
+    const userId = req.user._id;
+    const postId = req.body.postId;
+    const body = req.body.body;
+
+    const newComment = new Comment({
+      commentedPostId: postId,
+      author: userId,
+      body
+    });
+    if (req.file) newComment.image = req.file.buffer;
+    await newComment.save();
+    res.status(200).send();
+  },
+  (err, req, res, next) => {
+    res.send({ msg: err.message });
+  }
+);
 
 module.exports = router;
