058436c3163a4d6f2c2a965fe312195f0f3794f9
krzysztofwelc <krzysiekwelc18@gmail.com>
2020-03-02 11:44:31 +0100

    page me/posts

--- controllers/page.js
+++ controllers/page.js
@@ -16,14 +16,37 @@ router.get('/main/:page', auth, async (req, res) => {
         const posts = await Post.find({
             $or: query
         }, null, {
-            limit: 3,
-            skip: 3 * (parseInt(req.params.page, 10) - 1),
+            limit: 5,
+            skip: 5 * (parseInt(req.params.page, 10) - 1),
             sort: {
                 createdAt: -1
             }
         });
 
-        res.send(posts)
+        res.send({
+            posts,
+            user: req.user
+        })
+    } catch (e) {
+        res.status(500).send({
+            msg: e.message
+        });
+    }
+})
+
+router.get('/me/posts/:page', auth, async (req, res) => {
+    try {
+        const user = req.user;
+        const posts = await Post.find({
+            owner: user._id
+        }, null, {
+            limit: 5,
+            skip: 5 * (parseInt(req.params.page, 10) - 1),
+            sort: {
+                createdAt: -1
+            }
+        });
+        res.status(200).send(posts);
     } catch (e) {
         res.status(500).send({
             msg: e.message
