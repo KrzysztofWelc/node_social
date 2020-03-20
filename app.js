e21714ff77112ec2d1f3a99c32d0a3c1e2628a33
krzysztofwelc <krzysiekwelc18@gmail.com>
2020-03-20 20:58:02 +0100

    search preview

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 require("./db/conn");
 
@@ -10,18 +10,20 @@ const userController = require("./controllers/user");
 const postController = require("./controllers/post");
 const commentController = require("./controllers/comment");
 const pageController = require('./controllers/page');
+const searchController = require('./controllers/search');
 
 
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
 app.use("/user", userController);
 app.use("/post", postController);
 app.use('/comment', commentController);
 app.use('/page', pageController);
+app.use('/search', searchController);
 
 app.listen(PORT, e => {
   if (e) return console.log("==ERROR==", e);
---
+++ controllers/search.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const User = require('../db/models/User');
+
+router.get('/preview/:phrase', async(req, res)=>{
+    const phrase = req.params.phrase || '';
+
+    try {
+        const previews = await User.find({$or:[
+                {email: {$regex: phrase}},
+                {nickName: {$regex: phrase}}
+            ]}, null, {
+            limit: 5
+        });
+        res.status(200).send(previews);
+    }catch(e){
+        res.status(500).send({msg: e.message});
+    }
+});
+
+
+module.exports = router;
